5e11a42cf769bbd96b17720ce0a83487408d9108
SthablyM <mathapelofentse@icloud.com>
2024-11-12 17:17:01 +0000

    /0x01-Basic_authenticatio

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python3
-"""Error handler: Unauthorized"""
-from flask import Blueprint, abort
+""" Module of Index views
+"""
+from flask import jsonify, abort
+from api.v1.views import app_views
 
-index_blueprint = Blueprint('index', __name__)
 
-# Define the unauthorized endpoint
-@index_blueprint.route('/unauthorized', methods=['GET'])
-def unauthorized():
-    """the unauthorized endpoint"""
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
+def status() -> str:
+    """ GET /api/v1/status
+    Return:
+      - the status of the API
+    """
+    return jsonify({"status": "OK"})
+
+
+@app_views.route('/stats/', strict_slashes=False)
+def stats() -> str:
+    """ GET /api/v1/stats
+    Return:
+      - the number of each objects
+    """
+    from models.user import User
+    stats = {}
+    stats['users'] = User.count()
+    return jsonify(stats)
+
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return:
+      - raises a 401 error by using abort
+    """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - raises a 403 error by using abort
+    """
+    abort(403)
